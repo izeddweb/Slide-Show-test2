43ea09a4b7c29d3b3246893ed27a379a7202d652
izedweb <abzedd61@gmail.com>
2023-03-21 12:19:32 +0100

    solve problem js

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
-// ******************** Set Variable 
+// ******************** Set Variable
 
-const contentSlide = document.querySelector('.content');
-const contentImage = document.querySelector('.content img');
-const btns = document.querySelectorAll('button');
-const btnPrev =document.querySelector('.btn-prev');
-const btnNext =document.querySelector('.btn-next');
-const span = document.querySelectorAll('span');
-let sourceImage = contentImage.getAttribute('src');
+const contentSlide = document.querySelector(".content");
+const contentImage = document.querySelector(".content img");
+const btns = document.querySelectorAll("button");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+const span = document.querySelectorAll("span");
+let sourceImage = contentImage.getAttribute("src");
 
+// ******************** Set Function
 
-
-// ******************** Set Function 
-
-btns.forEach(function(ele){
+btns.forEach(function (ele) {
   let counter = 1;
-  let maxCounter = 5;
-  let minCounter = 1;
-  ele.onclick = function (){
-      if( ele.textContent == "next"){
-        counter++;
-        console.log(counter);
-        if( counter == maxCounter){
-          counter = 1;
-        }
-        contentImage.setAttribute('src',`/img-slide/${counter}.JPG`);
-      }else if( ele.textContent == "prev"){
-        counter -= 1;
-        console.log(counter);
-        if( counter <= minCounter){
-          counter = 5;
-          counter--;
-          console.log('tssss');
-        }
-        contentImage.setAttribute('src',`/img-slide/${counter}.JPG`)
+  ele.onclick = function () {
+    if (ele.textContent == "next") {
+      counter++;
+      if (counter > 4) {
+        counter = 1;
       }
-    console.log(contentImage.getAttribute('src'));
-  }
+      contentImage.setAttribute("src", `/img-slide/${counter}.JPG`);
+    } else if (ele.textContent == "prev") {
+      counter -= 1;
+      if (counter < 1) {
+        counter = 4;
+      }
+      contentImage.setAttribute("src", `/img-slide/${counter}.JPG`);
+    }
+  };
 });
-
-
