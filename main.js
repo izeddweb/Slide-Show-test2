251978f7d939938987b0b11ef9b19a8eca216f3b
izedweb <abzedd61@gmail.com>
2023-03-21 15:21:18 +0100

    working in js file 90% ok(span)

--- main.js
+++ main.js
@@ -5,28 +5,58 @@ const contentImage = document.querySelector(".content img");
 const btns = document.querySelectorAll("button");
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
-const span = document.querySelectorAll("span");
+const spans = document.querySelectorAll("span");
 let sourceImage = contentImage.getAttribute("src");
+let counter = 1;
+
 
 // ******************** Set Function
 
 btns.forEach(function (ele) {
-  let counter = 1;
+  // spansData = counter
   ele.onclick = function () {
     if (ele.textContent == "next") {
       counter++;
       if (counter > 4) {
         counter = 1;
       }
+      // changing img to slider  With Button next
       contentImage.setAttribute("src", `/img-slide/${counter}.JPG`);
     } else if (ele.textContent == "prev") {
       counter -= 1;
       if (counter < 1) {
         counter = 4;
       }
+      // changing img to slider  With Button prev
       contentImage.setAttribute("src", `/img-slide/${counter}.JPG`);
     }
+    // adding style to span
+    spans.forEach(function (span) {
+      let spansData = span.getAttribute("data-name");
+      if (spansData == counter) {
+        spans.forEach(function (span) {
+          span.classList.remove("span-style");
+        });
+        span.classList.add("span-style");
+        
+      }
+    });
   };
 });
-//   contunue in new branch test brch
 
+//   contunue in new branch test brch
+//  work in span-content
+
+spans.forEach(function(span){
+  let spansData = span.getAttribute('data-name');
+  span.onclick = function () {
+    spans.forEach(function(span){
+      
+      span.classList.remove('span-style')
+    })
+    counter = spansData
+    contentImage.setAttribute("src", `/img-slide/${counter}.JPG`);
+    span.classList.add('span-style')
+  }
+
+})
